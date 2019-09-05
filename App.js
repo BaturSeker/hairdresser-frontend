ebfe89e658052f0a30ee8088341fe59d80e3abd9
BaturSeker <baturseker06@gmail.com>
2019-09-06 00:25:11 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+
+    return (
+        <Text style={{fontWeight: 'bold'}}>
+            Deneme
+        </Text>
+    );
+
+
+};
+
+export default App;
